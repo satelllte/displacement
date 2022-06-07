6f77b37613b14fcb5e7c4cf2ccc94c3c5465aba5
George <lunaerxs@gmail.com>
2022-06-07 20:33:31 +0000

    _app.tsx: cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,25 +3,8 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { WASMContextProvider } from '../context/WASM'
 import { APP_NAME } from '../constants'
-import { useEffect } from 'react'
 
 const App = ({ Component, pageProps }: AppProps) => {
-
-  useEffect(() => {
-    // TODO: notify user that the app is available for offline use
-    // ...
-
-    // https://developer.chrome.com/docs/workbox/modules/workbox-window/#register-a-service-worker-and-notify-the-user-the-very-first-time-that-service-worker-is-active
-    // @ts-ignore
-    const wb = window.workbox
-    // @ts-ignore
-    wb.addEventListener('activated', (event) => {
-      if (!event.isUpdate) {
-        console.info('WB | activated | event: ', event)
-      }
-    })
-  }, [])
-
   return (
     <WASMContextProvider>
       <Head>
