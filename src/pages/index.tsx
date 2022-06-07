2e000fefc28c3a7095260a2ea3b47f2b987fcc5f
George <lunaerxs@gmail.com>
2022-06-07 22:51:04 +0000

    layout: 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,23 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import { WASMExample } from '../components/WASMExample'
 
 const Home: NextPage = () => {
+  const [settingsOpened, setSettingsOpened] = useState(false);
   return (
-    <>
-      <main className='min-h-screen p-4 flex flex-col'>
-        <h1 className='text-3xl sm:text-4xl lg:text-5xl text-center font-bold my-8 lg:my-12'>
-          Welcome to <a href="https://nextjs.org">Next.js</a> with WebAssembly!
-        </h1>
-        <WASMExample/>
+    <div className='absolute inset-0 overflow-hidden flex flex-col'>
+      <header className='p-4 bg-red-900'>Header</header>
+      <main className='relative flex-1 flex justify-between flex-col md:flex-row'>
+        <section className='p-4 flex-1 bg-green-900'>Canvas</section>
+        <section className={`p-4 fixed inset-x-0 h-2/3 bg-blue-900 transition-transform ease-out duration-150 ${settingsOpened ? '-translate-y-full' : ''} transform-gpu top-full md:h-auto md:static md:flex-1`}>
+          Settings
+          <button className={`md:hidden`} onClick={() => setSettingsOpened(false)}>Close settings</button>
+        </section>
+        <div className='p-4 text-center md:hidden'>
+          <button onClick={() => setSettingsOpened(true)}>Open Settings</button>
+        </div>
       </main>
-    </>
+    </div>
   )
 }
 
