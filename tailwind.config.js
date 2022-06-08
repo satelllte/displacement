0ac96d3972c3a03be87d2f61315a0dfafd14470d
George <lunaerxs@gmail.com>
2022-06-08 19:50:42 +0000

    css improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      transitionTimingFunction: {
+        'ease-out-quad': 'cubic-bezier(0.5, 1, 0.89, 1)',
+      },
+    },
   },
   plugins: [],
 }
