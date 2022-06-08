303789ebf6f83b1f8ec9581f88e360cc918127de
George <lunaerxs@gmail.com>
2022-06-08 20:24:26 +0000

    viewport: scalable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       <SizeContextProvider>
         <Head>
           <title>{APP_NAME}</title>
-          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
         </Head>
         <Component {...pageProps} />
       </SizeContextProvider>
