c5726fe74b0129bbfc2707c77c5a1a58e302835c
George <lunaerxs@gmail.com>
2022-06-19 23:05:33 +0000

    stories.Button: default args

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -6,12 +6,12 @@ import { Button } from './Button'
 export default {
   title: 'Button',
   component: Button,
-  argTypes: {},
+  args: {
+    children: 'Activate'
+  },
 } as ComponentMeta<typeof Button>
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 
 export const Primary = Template.bind({})
-Primary.args = {
-  children: 'My Button'
-}
+Primary.args = {}
