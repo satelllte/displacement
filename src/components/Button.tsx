224e04d585c85726591b1ccb3bbb7c720011f70a
George <lunaerxs@gmail.com>
2022-06-19 22:59:57 +0000

    test Button: styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ export const Button: React.FC<ButtonProps> = ({
   children,
 }) => {
   return (
-    <button className='p-2 bg-green-600'>
+    <button className='px-6 py-2 transition-colors text-white rounded-xl bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300'>
       {children}
     </button>
   )
