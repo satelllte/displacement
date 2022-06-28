4d9bc6aa48593f30d857fd98c8c07bc0cbf155fe
George <lunaerxs@gmail.com>
2022-06-28 16:37:18 +0000

    Options component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import { Canvas } from '../components/Canvas';
+import { Options } from '../components/Options';
 import { WASMContext } from '../context/WASMContext';
 
 const Home: NextPage = () => {
@@ -23,10 +24,7 @@ const Home: NextPage = () => {
           </div>
         </section>
         <section className='mt-16 h-[calc(100vh-theme(spacing.16))] relative md:flex-1'>
-          <div className='absolute inset-4 flex flex-col justify-between'>
-            <h2 className='mb-8 text-xl'>Options</h2>
-            <button className='p-4 w-full bg-neutral-800/50 hover:bg-neutral-800/75 active:bg-neutral-800 transition-all outline outline-2 outline-transparent focus:outline-neutral-800 drop-shadow-xl'>Render</button>
-          </div>
+          <Options/>
         </section>
       </div>
     </>
