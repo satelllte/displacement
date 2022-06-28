3b047622adbfacfb56617e07d8fcbe5376b4d25e
George <lunaerxs@gmail.com>
2022-06-28 19:13:52 +0000

    CanvasContext

--- src/components/CanvasContainer.tsx
+++ src/components/CanvasContainer.tsx
@@ -1,16 +1,29 @@
-import { WASMContext } from '../context/WASMContext'
-import { Canvas } from './Canvas'
+import React from 'react'
+import { DEFAULT_HEIGHT, DEFAULT_WIDTH } from '../constants'
+import { CanvasContext } from '../context/CanvasContext'
 
 export const CanvasContainer = () => {
+  const canvasRef = React.useContext(CanvasContext)
+
+  React.useEffect(() => {
+    if (!canvasRef.current) {
+      throw new Error('canvasRef is not accessible')
+    }
+
+    const canvas = canvasRef.current
+
+    const width = DEFAULT_WIDTH
+    const height = DEFAULT_HEIGHT
+    
+    canvas.width = width
+    canvas.height = height
+  }, [])
+
   return (
     <div className='absolute inset-4 overflow-y-auto'>
       <div className='absolute inset-0 flex justify-center items-center'>
         <div className='relative flex items-center w-full h-full max-w-[500px] max-h-[500px]'>
-          <WASMContext.Consumer>
-            {({ wasm }) => {
-              return wasm ? <Canvas wasm={wasm}/> : null
-            }}
-          </WASMContext.Consumer>
+          <canvas ref={canvasRef} className='drop-shadow-xl absolute max-h-full max-w-full bg-neutral-900' />
         </div>
       </div>
     </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,19 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { WASMContextProvider } from '../context/WASMContext'
 import { APP_NAME } from '../constants'
+import { CanvasContextProvider } from '../context/CanvasContext'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <WASMContextProvider>
-      <Head>
-        <title>{APP_NAME}</title>
-        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
-      </Head>
-      <Component {...pageProps} />
-    </WASMContextProvider>
+    <CanvasContextProvider>
+      <WASMContextProvider>
+        <Head>
+          <title>{APP_NAME}</title>
+          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
+        </Head>
+        <Component {...pageProps} />
+      </WASMContextProvider>
+    </CanvasContextProvider>
   )
 }
 
