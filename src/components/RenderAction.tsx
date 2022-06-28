42d3d81232a363a6d03057ed786e42a1e810e86a
George <lunaerxs@gmail.com>
2022-06-28 21:21:20 +0000

    first rectangles render

--- src/components/RenderAction.tsx
+++ src/components/RenderAction.tsx
@@ -29,6 +29,24 @@ export const RenderAction = () => {
       width,
       height,
     )
+
+    for (let i = 0; i < 100; i++) {
+      const x0 = randomInt(0, width - 1)
+      const y0 = randomInt(0, height - 1)
+      const x1 = Math.min(x0 + randomInt(10, 100), width - 1)
+      const y1 = Math.min(y0 + randomInt(10, 100), height - 1)
+      wasm.fillRect(
+        randomInt(0x00, 0x99),
+        randomInt(0x00, 0x99),
+        randomInt(0x00, 0x99),
+        x0,
+        y0,
+        x1,
+        y1,
+        width,
+      )
+    }
+
     imageData.data.set(pixels)
     ctx2d.putImageData(imageData, 0, 0)
   }
