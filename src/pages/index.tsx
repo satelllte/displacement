1d0b104eeb5c5f702c9e487f06ea381beb41721b
George <lunaerxs@gmail.com>
2022-06-28 16:35:22 +0000

    btn: drop shadow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
         <section className='mt-16 h-[calc(100vh-theme(spacing.16))] relative md:flex-1'>
           <div className='absolute inset-4 flex flex-col justify-between'>
             <h2 className='mb-8 text-xl'>Options</h2>
-            <button className='p-4 w-full bg-neutral-800/50 hover:bg-neutral-800/75 active:bg-neutral-800 transition-all outline outline-2 outline-transparent focus:outline-neutral-800'>Render</button>
+            <button className='p-4 w-full bg-neutral-800/50 hover:bg-neutral-800/75 active:bg-neutral-800 transition-all outline outline-2 outline-transparent focus:outline-neutral-800 drop-shadow-xl'>Render</button>
           </div>
         </section>
       </div>
