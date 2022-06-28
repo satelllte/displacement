63048d550c1a90332d16c8191bb8325e9760dc00
George <lunaerxs@gmail.com>
2022-06-29 01:52:24 +0300

    `next build` CI optimizations (#56)
    next build CI optimization

--- next.config.js
+++ next.config.js
@@ -7,6 +7,14 @@ const nextConfig = withPWA({
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
   },
+  eslint: {
+    // Ignore eslint checks during `next build` run on CI, because it's checked on a separate step before
+    ignoreDuringBuilds: process.env.NEXT_CI_BUILD === 'true',
+  },
+  typescript: {
+    // Ignore typescript checks during `next build` run on CI, because it's checked on a separate step before
+    ignoreBuildErrors: process.env.NEXT_CI_BUILD === 'true',
+  },
   webpack: (config) => {
     config.experiments.asyncWebAssembly = true
     return config
