4be9ef0419b6e5d7b95f8f9328dcfcc4ef757a93
George <lunaerxs@gmail.com>
2022-06-30 20:41:09 +0000

    remove unused `WASMContext`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { WASMContextProvider } from '../context/WASMContext'
 import { APP_NAME } from '../constants'
 import { CanvasContextProvider } from '../context/CanvasContext'
 import { WASMWorkerContextProvider } from '../context/WASMWorkerContext'
@@ -10,13 +9,11 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <CanvasContextProvider>
       <WASMWorkerContextProvider>
-        <WASMContextProvider>
-          <Head>
-            <title>{APP_NAME}</title>
-            <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
-          </Head>
-          <Component {...pageProps} />
-        </WASMContextProvider>
+        <Head>
+          <title>{APP_NAME}</title>
+          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
+        </Head>
+        <Component {...pageProps} />
       </WASMWorkerContextProvider>
     </CanvasContextProvider>
   )
