c703590d498e7391979427647767c24342758952
George <lunaerxs@gmail.com>
2022-06-30 21:59:32 +0000

    Range: base

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,9 +1,28 @@
+import React from 'react'
+import { Range } from './Range'
 import { RenderAction } from './RenderAction'
 
 export const Options = () => {
+  const [iterations, setIterations] = React.useState<number>(50)
+
   return (
     <div className='absolute inset-4 flex flex-col justify-between'>
-      <h2 className='mb-8 text-xl'>Options</h2>
+      <div className='flex flex-col'>
+        <h2 className='mb-8 text-xl'>Options</h2>
+        <label
+          htmlFor='iterations'
+          className='text-sm text-neutral-300'
+        >
+          Iterations
+        </label>
+        <Range
+          id='iterations'
+          min={10}
+          max={1000}
+          value={iterations}
+          onValueChange={setIterations}
+        />
+      </div>
       <RenderAction/>
     </div>
   )
