586093db50b37eb802d1e3ca355bfc4bdfcc7fba
George <lunaerxs@gmail.com>
2022-06-30 17:56:02 +0000

    set 4k as default resolution

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ export const APP_DESCRIPTION = 'Displacement maps generator'
 
 export const MAX_WIDTH = 8196
 export const MAX_HEIGHT = 8196
-export const DEFAULT_WIDTH = MAX_WIDTH
-export const DEFAULT_HEIGHT = MAX_HEIGHT
+export const DEFAULT_WIDTH = 4098
+export const DEFAULT_HEIGHT = 4098
