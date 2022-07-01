f6deb6943cd7896098d7fee4aa0a0ccec038d45d
George <lunaerxs@gmail.com>
2022-07-01 15:03:12 +0000

    fix: maximum dimensions

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const APP_NAME = 'DISPLACEMENT'
 export const APP_DESCRIPTION = 'Displacement maps generator'
 
-export const MAX_WIDTH = 8196
-export const MAX_HEIGHT = 8196
-export const DEFAULT_WIDTH = 4098
-export const DEFAULT_HEIGHT = 4098
+export const MAX_WIDTH = 8192
+export const MAX_HEIGHT = 8192
+export const DEFAULT_WIDTH = 4096
+export const DEFAULT_HEIGHT = 4096
