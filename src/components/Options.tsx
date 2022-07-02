7fa9e7ff2e9c54af4d15c7f2de9f9cc8ea2a9b45
George <lunaerxs@gmail.com>
2022-07-02 22:17:30 +0000

    unified `Option` component

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { BackgroundBrightnessOption } from './BackgroundBrightnessOption'
-import { IterationsOption } from './IterationsOption'
+import { backgroundBrightnessState, iterationsState } from '../state'
+import { Option } from './Option'
 import { RenderAction } from './RenderAction'
 
 export const Options = () => {
@@ -8,8 +8,20 @@ export const Options = () => {
     <div className='absolute inset-4 flex flex-col justify-between'>
       <div className='flex flex-col'>
         <h2 className='mb-8 text-xl'>Options</h2>
-        <IterationsOption/>
-        <BackgroundBrightnessOption/>
+        <Option
+          id='iterations'
+          label='Iterations'
+          min={10}
+          max={2000}
+          state={iterationsState}
+        />
+        <Option
+          id='backgroundBrightness'
+          label='Background Brightness'
+          min={0x00}
+          max={0xFF}
+          state={backgroundBrightnessState}
+        />
       </div>
       <RenderAction/>
     </div>
