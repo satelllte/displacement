6a01c85b9036773bbb706c73040c7583c4508c27
George <lunaerxs@gmail.com>
2022-07-06 20:36:40 +0000

    use `.js` format for eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
-{
+module.exports = {
   "extends": "next/core-web-vitals",
   "rules": {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": [
       "warn", {
-        "additionalHooks": "(useRecoilCallback|useRecoilTransaction_UNSTABLE)"
+        "additionalHooks": "(useRecoilCallback|useRecoilTransaction_UNSTABLE)" // https://recoiljs.org/docs/introduction/installation/#eslint
       }
     ]
   }
