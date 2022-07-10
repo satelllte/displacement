356d6d56635a2b4d00a437fa5a3ea8ab71598615
George <lunaerxs@gmail.com>
2022-07-10 11:39:47 +0000

    remove wasm-related `src` files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,20 +5,17 @@ import { RecoilRoot } from 'recoil'
 import { APP_NAME } from '../constants'
 import { CanvasContextProvider } from '../context/CanvasContext'
 import { RendererContextProvider } from '@/context/RendererContext'
-import { WASMContextProvider } from '../context/WASMContext'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <RecoilRoot>
       <CanvasContextProvider>
         <RendererContextProvider>
-          <WASMContextProvider>
-            <Head>
-              <title>{APP_NAME}</title>
-              <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
-            </Head>
-            <Component {...pageProps} />
-          </WASMContextProvider>
+          <Head>
+            <title>{APP_NAME}</title>
+            <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
+          </Head>
+          <Component {...pageProps} />
         </RendererContextProvider>
       </CanvasContextProvider>
     </RecoilRoot>
