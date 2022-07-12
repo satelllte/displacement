c6ef8633d52ca78017452351673e3adab834f739
George <lunaerxs@gmail.com>
2022-07-12 16:53:00 +0000

    move `iterationsPerFrame` to `RenderOptions`

--- src/graphics/helpers/render.ts
+++ src/graphics/helpers/render.ts
@@ -5,14 +5,21 @@ export const render = (
   ctx: CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D,
   width: number,
   height: number,
-  options: RenderOptions,
+  {
+    iterations,
+    iterationsPerFrame = 50,
+    backgroundBrightness,
+    rectBrightnessMin,
+    rectBrightnessMax,
+    rectAlphaMin,
+    rectAlphaMax,
+  }: RenderOptions,
   onComplete: () => void,
 ) => {
-  ctx.fillStyle = `rgb(${options.backgroundBrightness}, ${options.backgroundBrightness}, ${options.backgroundBrightness})`
+  ctx.fillStyle = `rgb(${backgroundBrightness}, ${backgroundBrightness}, ${backgroundBrightness})`
   ctx.fillRect(0, 0, width, height)
 
   let iteration = 0
-  const frameIterations = 50
 
   const minRectWidth = Math.round(width / 24)
   const maxRectWidth = Math.round(width / 8)
@@ -20,11 +27,11 @@ export const render = (
   const maxRectHeight = Math.round(height / 8)
 
   const draw = () => {
-    const last = Math.min(iteration + frameIterations, options.iterations)
+    const last = Math.min(iteration + iterationsPerFrame, iterations)
     
     while (iteration < last) {
-      const brightness = randomInt(options.rectBrightnessMin, options.rectBrightnessMax)
-      const alpha = randomInt(options.rectAlphaMin, options.rectAlphaMax) / 0xFF
+      const brightness = randomInt(rectBrightnessMin, rectBrightnessMax)
+      const alpha = randomInt(rectAlphaMin, rectAlphaMax) / 0xFF
       const rectWidth = randomInt(minRectWidth, maxRectWidth)
       const rectHeight = randomInt(minRectHeight, maxRectHeight)
       ctx.fillStyle = `rgba(${brightness}, ${brightness}, ${brightness}, ${alpha})`
@@ -37,7 +44,7 @@ export const render = (
       iteration++
     }
     
-    if (iteration < options.iterations) {
+    if (iteration < iterations) {
       requestAnimationFrame(draw)
       return
     }
--- src/graphics/types.d.ts
+++ src/graphics/types.d.ts
@@ -1,5 +1,6 @@
 export interface RenderOptions {
   iterations: number
+  iterationsPerFrame?: number
   backgroundBrightness: number
   rectBrightnessMin: number
   rectBrightnessMax: number
